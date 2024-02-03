db1366801a84f4ec68a3b677eec0278676410bd0
Leo Halbritter <e12020647@student.tuwien.ac.at>
2024-02-03 22:15:48 +0100

    Testing out monaco editor with angular.

--- shadertoy/src/app/app.component.ts
+++ shadertoy/src/app/app.component.ts
@@ -1,27 +1,27 @@
 import {AfterViewInit, Component, ElementRef, NgZone, ViewChild} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {Engine} from "babylonjs";
+import {Engine, Effect} from "babylonjs";
 import {MyFirstScene} from "./scenes/MyFirstScene";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 
 export class AppComponent implements AfterViewInit {
   @ViewChild('canvas', {static: true}) private canvas?: ElementRef<HTMLCanvasElement>;
   private engine?: Engine;
   private scene?: MyFirstScene;
+  editorOptions = {theme: 'vs-dark', language: 'javascript'};
+  code: string = 'function x() {\nconsole.log("Hello world!");\n}';
+
   //private ngZone: NgZone = new NgZone([]);
   constructor(private ngZone: NgZone) {}
   ngAfterViewInit(): void {
     if (this.canvas) {
       this.engine = new Engine(this.canvas.nativeElement);
       this.scene = new MyFirstScene(this.engine);
-      
+
       this.ngZone?.runOutsideAngular(() => {
         this.engine?.runRenderLoop(() => {
           this.scene?.render();
@@ -29,4 +29,6 @@ export class AppComponent implements AfterViewInit {
       });
     }
   }
+
+  protected readonly Effect = Effect;
 }
