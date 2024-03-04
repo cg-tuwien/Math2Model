367c5ce901d503ef78a3e68c0da48e04305508ba
Stefnotch <brandmairstefan@gmail.com>
2024-03-04 19:47:49 +0100

    Try out unocss

---
+++ uno.config.ts
@@ -0,0 +1,7 @@
+// uno.config.ts
+import { defineConfig } from "@unocss/vite";
+import { presetUno } from "@unocss/preset-uno";
+
+export default defineConfig({
+  presets: [presetUno()],
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from "node:url";
 import Components from "unplugin-vue-components/vite";
 import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
+import UnoCSS from "@unocss/vite";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
@@ -12,6 +13,7 @@ export default defineConfig({
     Components({
       resolvers: [NaiveUiResolver()],
     }),
+    UnoCSS(),
   ],
   resolve: {
     alias: {
