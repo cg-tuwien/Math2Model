d5f3dcefff3e919ec6cbbfe567890f4875710301
Stefnotch <brandmairstefan@gmail.com>
2024-05-26 20:49:06 +0200

    Fix too-many-buffers

--- parametric-renderer-core/renderer-core/src/application.rs
+++ parametric-renderer-core/renderer-core/src/application.rs
@@ -169,7 +169,7 @@ pub struct GpuApplication {
     render_buffer: Vec<TypedBuffer<compute_patches::RenderBuffer>>,
     indirect_compute_buffer_initial: compute_patches::DispatchIndirectArgs,
     indirect_compute_buffer: [TypedBuffer<compute_patches::DispatchIndirectArgs>; 2],
-    indirect_draw_buffers: Vec<TypedBuffer<copy_patches::DrawIndexedIndirectArgs>>,
+    indirect_draw_buffers: TypedBuffer<copy_patches::DrawIndexedBuffers>,
     meshes: Vec<Mesh>,
     threshold_factor: f32, // TODO: Make this smaller
     cursor_capture: WindowCursorCapture,
@@ -530,26 +530,33 @@ impl GpuApplication {
             ),
         ];
 
-        let indirect_draw_buffers = meshes
+        let indirect_draw_data = meshes
             .iter()
-            .zip(patch_sizes())
-            .map(|(mesh, patch_size)| {
-                TypedBuffer::new_storage(
-                    &device,
-                    &format!("Indirect Draw Buffer {}", patch_size),
-                    &copy_patches::DrawIndexedIndirectArgs {
-                        index_count: mesh.num_indices,
-                        instance_count: 0, // Our shader sets this
-                        first_index: 0,
-                        base_vertex: 0,
-                        first_instance: 0,
-                    },
-                    wgpu::BufferUsages::INDIRECT
-                        | wgpu::BufferUsages::STORAGE
-                        | wgpu::BufferUsages::COPY_SRC,
-                )
+            .map(|mesh| {
+                copy_patches::DrawIndexedIndirectArgs {
+                    index_count: mesh.num_indices,
+                    instance_count: 0, // Our shader sets this
+                    first_index: 0,
+                    base_vertex: 0,
+                    first_instance: 0,
+                }
             })
-            .collect::<Result<Vec<_>, _>>()?;
+            .collect::<Vec<_>>();
+
+        let indirect_draw_buffers = TypedBuffer::new_storage(
+            &device,
+            &"Indirect Draw Buffers",
+            &copy_patches::DrawIndexedBuffers {
+                indirect_draw_2: indirect_draw_data[0],
+                indirect_draw_4: indirect_draw_data[1],
+                indirect_draw_8: indirect_draw_data[2],
+                indirect_draw_16: indirect_draw_data[3],
+                indirect_draw_32: indirect_draw_data[4],
+            },
+            wgpu::BufferUsages::INDIRECT
+                | wgpu::BufferUsages::STORAGE
+                | wgpu::BufferUsages::COPY_SRC,
+        )?;
 
         let copy_patches_pipeline =
             device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
@@ -567,11 +574,7 @@ impl GpuApplication {
                 render_buffer_8: render_buffer[2].as_entire_buffer_binding(),
                 render_buffer_16: render_buffer[3].as_entire_buffer_binding(),
                 render_buffer_32: render_buffer[4].as_entire_buffer_binding(),
-                indirect_draw_2: indirect_draw_buffers[0].as_entire_buffer_binding(),
-                indirect_draw_4: indirect_draw_buffers[1].as_entire_buffer_binding(),
-                indirect_draw_8: indirect_draw_buffers[2].as_entire_buffer_binding(),
-                indirect_draw_16: indirect_draw_buffers[3].as_entire_buffer_binding(),
-                indirect_draw_32: indirect_draw_buffers[4].as_entire_buffer_binding(),
+                indirect_draw: indirect_draw_buffers.as_entire_buffer_binding(),
             },
         );
 
@@ -803,17 +806,30 @@ impl GpuApplication {
                 },
             );
             render_pass.set_pipeline(&self.render_pipeline);
-            for ((bind_group_1, mesh), indirect_draw_buffer) in self
+            let indirect_draw_offsets = [
+                std::mem::offset_of!(copy_patches::DrawIndexedBuffers, indirect_draw_2)
+                    as wgpu::BufferAddress,
+                std::mem::offset_of!(copy_patches::DrawIndexedBuffers, indirect_draw_4)
+                    as wgpu::BufferAddress,
+                std::mem::offset_of!(copy_patches::DrawIndexedBuffers, indirect_draw_8)
+                    as wgpu::BufferAddress,
+                std::mem::offset_of!(copy_patches::DrawIndexedBuffers, indirect_draw_16)
+                    as wgpu::BufferAddress,
+                std::mem::offset_of!(copy_patches::DrawIndexedBuffers, indirect_draw_32)
+                    as wgpu::BufferAddress,
+            ];
+            for ((bind_group_1, mesh), buffer_offset) in self
                 .render_bind_group_1
                 .iter()
                 .zip(self.meshes.iter())
-                .zip(self.indirect_draw_buffers.iter())
+                .zip(indirect_draw_offsets)
             {
                 shader::set_bind_groups(&mut render_pass, &self.render_bind_group_0, &bind_group_1);
                 render_pass.set_vertex_buffer(0, mesh.vertex_buffer.slice(..));
                 render_pass
                     .set_index_buffer(mesh.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
-                render_pass.draw_indexed_indirect(indirect_draw_buffer.buffer(), 0)
+                render_pass
+                    .draw_indexed_indirect(self.indirect_draw_buffers.buffer(), buffer_offset)
             }
         }
 
--- parametric-renderer-core/shaders/CopyPatches.wgsl
+++ parametric-renderer-core/shaders/CopyPatches.wgsl
@@ -53,22 +53,19 @@ struct DrawIndexedBuffers {
 };
 
 @group(0) @binding(0) var<storage, read> render_buffer_2 : RenderBufferRead;
-@group(0) @binding(1) var<storage, read_write> indirect_draw_2: DrawIndexedIndirectArgs;
-@group(0) @binding(2) var<storage, read> render_buffer_4 : RenderBufferRead;
-@group(0) @binding(3) var<storage, read_write> indirect_draw_4: DrawIndexedIndirectArgs;
-@group(0) @binding(4) var<storage, read> render_buffer_8 : RenderBufferRead;
-@group(0) @binding(5) var<storage, read_write> indirect_draw_8: DrawIndexedIndirectArgs;
-@group(0) @binding(6) var<storage, read> render_buffer_16 : RenderBufferRead;
-@group(0) @binding(7) var<storage, read_write> indirect_draw_16: DrawIndexedIndirectArgs;
-@group(0) @binding(8) var<storage, read> render_buffer_32 : RenderBufferRead;
-@group(0) @binding(9) var<storage, read_write> indirect_draw_32: DrawIndexedIndirectArgs;
+@group(0) @binding(1) var<storage, read> render_buffer_4 : RenderBufferRead;
+@group(0) @binding(2) var<storage, read> render_buffer_8 : RenderBufferRead;
+@group(0) @binding(3) var<storage, read> render_buffer_16 : RenderBufferRead;
+@group(0) @binding(4) var<storage, read> render_buffer_32 : RenderBufferRead;
+
+@group(0) @binding(5) var<storage, read_write> indirect_draw: DrawIndexedBuffers;
 
 /// Copies the render buffer sizes to indirect draws
 @compute @workgroup_size(1, 1, 1)
 fn main(@builtin(global_invocation_id) global_id : vec3<u32>) {
-  indirect_draw_2.instance_count = render_buffer_2.patches_length;
-  indirect_draw_4.instance_count = render_buffer_4.patches_length;
-  indirect_draw_8.instance_count = render_buffer_8.patches_length;
-  indirect_draw_16.instance_count = render_buffer_16.patches_length;
-  indirect_draw_32.instance_count = render_buffer_32.patches_length;
+  indirect_draw.indirect_draw_2.instance_count = render_buffer_2.patches_length;
+  indirect_draw.indirect_draw_4.instance_count = render_buffer_4.patches_length;
+  indirect_draw.indirect_draw_8.instance_count = render_buffer_8.patches_length;
+  indirect_draw.indirect_draw_16.instance_count = render_buffer_16.patches_length;
+  indirect_draw.indirect_draw_32.instance_count = render_buffer_32.patches_length;
 }
