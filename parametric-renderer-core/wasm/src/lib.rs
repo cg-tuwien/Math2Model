2cfad666f5984e1297033ab696d0d256801a0fa7
Stefnotch <brandmairstefan@gmail.com>
2024-07-10 12:45:29 +0200

    Bump deps

--- parametric-renderer-core/wasm/src/lib.rs
+++ parametric-renderer-core/wasm/src/lib.rs
@@ -1,20 +1,20 @@
 mod application;
 
-use tracing_subscriber::fmt::{format::Pretty, time::UtcTime};
+use tracing_subscriber::fmt::{format::JsonFields, time::UtcTime};
 use tracing_subscriber::prelude::*;
 use tracing_web::{performance_layer, MakeWebConsoleWriter};
 use wasm_bindgen::prelude::*;
 use web_sys::HtmlCanvasElement;
 
 #[wasm_bindgen(start)]
-pub fn run() { 
+pub fn run() {
     console_error_panic_hook::set_once();
     // https://crates.io/crates/tracing-web
     let fmt_layer = tracing_subscriber::fmt::layer()
         .with_ansi(false)
         .with_timer(UtcTime::rfc_3339())
         .with_writer(MakeWebConsoleWriter::new());
-    let perf_layer = performance_layer().with_details_from_fields(Pretty::default());
+    let perf_layer = performance_layer().with_details_from_fields(JsonFields::default());
 
     tracing_subscriber::registry()
         .with(fmt_layer)
