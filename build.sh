9d0de899fe89a91ff3ce13a46ed97a4a9345bf16
stefnotch <stefnotch@users.noreply.github.com>
2024-07-25 19:34:30 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# Depends on Node.js v20 or newer, npm and Rust
+cd parametric-renderer-core\
+cargo run --bin copy-includes
+# Can also run arbitrary code, because of npm postinstall hooks
+npm ci
+npm run build
+
+cd ..
+npm ci
+npm run build
