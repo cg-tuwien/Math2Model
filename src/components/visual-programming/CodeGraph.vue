40278fb0e27083c0ae1585aa4a212f8d9303eff9
Leo Halbritter <e12020647@student.tuwien.ac.at>
2024-08-14 12:28:21 +0200

    Started implementing nodes that return code and references. Code execution order should be correct now

--- src/components/visual-programming/CodeGraph.vue
+++ src/components/visual-programming/CodeGraph.vue
@@ -21,7 +21,7 @@ import {
 import {
   NumberNode,
   reteSocket as socket,
-  AddNode,
+  MathOpNode,
   Vector2Node,
   Seperate2Node,
   Join2Node,
@@ -29,6 +29,8 @@ import {
 } from "@/vpnodes/nodes";
 import { DataflowEngine } from "rete-engine";
 import { DockPlugin, DockPresets } from "rete-dock-plugin";
+import { structures } from "rete-structures";
+import { root } from "postcss";
 
 const container = ref<HTMLElement | null>(null);
 
@@ -40,7 +42,7 @@ onMounted(() => {
 
 type Nodes =
   | NumberNode
-  | AddNode
+  | MathOpNode
   | Vector2Node
   | Seperate2Node
   | Join2Node
@@ -51,21 +53,21 @@ class Connection<
 > extends ClassicPreset.Connection<A, B> {}
 
 type Conns =
-  | Connection<NumberNode, AddNode>
-  | Connection<AddNode, AddNode>
+  | Connection<NumberNode, MathOpNode>
+  | Connection<MathOpNode, MathOpNode>
   | Connection<Vector2Node, Seperate2Node>
-  | Connection<Seperate2Node, AddNode>
+  | Connection<Seperate2Node, MathOpNode>
   | Connection<Seperate2Node, Join2Node>
   | Connection<Join2Node, Seperate2Node>
   | Connection<NumberNode, Join2Node>
   | Connection<Vector3Node, Seperate2Node>;
 
+type Schemes = GetSchemes<Nodes, Conns>;
+
+const editor = new NodeEditor<Schemes>();
+const engine = new DataflowEngine<Schemes>();
+
 async function createEditor() {
-  type Schemes = GetSchemes<Nodes, Conns>;
-
-  const editor = new NodeEditor<Schemes>();
-  const engine = new DataflowEngine<Schemes>();
-
   type AreaExtra = VueArea2D<Schemes> | ContextMenuExtra;
 
   const area = new AreaPlugin<Schemes, AreaExtra>(
@@ -81,7 +83,23 @@ async function createEditor() {
   const contextMenu = new ContextMenuPlugin<Schemes>({
     items: ContextMenuPresets.classic.setup([
       ["Number", () => new NumberNode()],
-      ["Add", () => new AddNode((c) => area.update("control", c.id))],
+      ["Add", () => new MathOpNode("+", (c) => area.update("control", c.id))],
+      [
+        "Subtract",
+        () => new MathOpNode("-", (c) => area.update("control", c.id)),
+      ],
+      [
+        "Multiply",
+        () => new MathOpNode("*", (c) => area.update("control", c.id)),
+      ],
+      [
+        "Divide",
+        () => new MathOpNode("/", (c) => area.update("control", c.id)),
+      ],
+      [
+        "Modulo",
+        () => new MathOpNode("%", (c) => area.update("control", c.id)),
+      ],
       ["Vector2", () => new Vector2Node((c) => area.update("control", c.id))],
       ["Vector3", () => new Vector3Node((c) => area.update("control", c.id))],
       [
@@ -116,7 +134,11 @@ async function createEditor() {
   area.use(dock);
 
   dock.add(() => new NumberNode());
-  dock.add(() => new AddNode((c) => area.update("control", c.id)));
+  dock.add(() => new MathOpNode("+", (c) => area.update("control", c.id)));
+  dock.add(() => new MathOpNode("-", (c) => area.update("control", c.id)));
+  dock.add(() => new MathOpNode("*", (c) => area.update("control", c.id)));
+  dock.add(() => new MathOpNode("/", (c) => area.update("control", c.id)));
+  dock.add(() => new MathOpNode("%", (c) => area.update("control", c.id)));
   dock.add(() => new Vector2Node((c) => area.update("control", c.id)));
   dock.add(() => new Vector3Node((c) => area.update("control", c.id)));
   dock.add(() => new Seperate2Node((c) => area.update("control", c.id)));
@@ -128,11 +150,96 @@ async function createEditor() {
 
       editor
         .getNodes()
-        .filter((n) => n instanceof AddNode)
+        .filter((n) => n instanceof MathOpNode)
         .forEach((n) => engine.fetch(n.id));
+      logCode();
     }
+
     return context;
   });
+}
+
+async function getNodesCode(node: Nodes) {
+  const nodeData = await engine.fetch(node.id);
+  let fullCode = "";
+  if (node instanceof Seperate2Node) {
+    fullCode += "\t" + nodeData.x.code + "\n";
+    fullCode += "\t" + nodeData.y.code + "\n";
+  } else {
+    fullCode += "\t" + nodeData.value.code + "\n";
+  }
+
+  return fullCode;
+}
+
+async function logCode() {
+  const graph = structures(editor);
+  const rootNodes = graph.roots().nodes();
+  const leafNodes = graph.leaves().nodes();
+  const allNodes = graph.nodes();
+  let visited = [];
+  let fullCode = "{\n";
+  for (let node of rootNodes) {
+    visited.push(node.id);
+    fullCode += await getNodesCode(node);
+  }
+
+  let ind = 0;
+  let currentNode = rootNodes[ind];
+  let rootsDone = false;
+  while (!leafNodes.includes(currentNode)) {
+    const outgoers = graph.outgoers(currentNode.id).nodes();
+    if (!visited.includes(currentNode.id)) {
+      fullCode += await getNodesCode(currentNode);
+      visited.push(currentNode.id);
+    }
+
+    for (let node of outgoers) {
+      if (visited.includes(node.id)) continue;
+      visited.push(node.id);
+
+      fullCode += await getNodesCode(node);
+    }
+
+    if (!rootsDone) {
+      ind += 1;
+      if (ind >= rootNodes.length) {
+        ind = 0;
+        rootsDone = true;
+        currentNode = allNodes[ind];
+      } else {
+        currentNode = rootNodes[ind];
+      }
+    } else {
+      ind += 1;
+      if (ind >= allNodes.length) {
+        break;
+      }
+      currentNode = allNodes[ind];
+    }
+  }
+
+  for (let node of leafNodes) {
+    if (visited.includes(node.id)) continue;
+    visited.push(node.id);
+
+    fullCode += await getNodesCode(node);
+  }
+  /**const rest = graph.nodes();
+  for (let node of rest) {
+    if (rootNodes.includes(node)) continue;
+    const nodeData = await engine.fetch(node.id);
+
+    if (node instanceof Seperate2Node) {
+      fullCode += "\t" + nodeData.x.code + "\n";
+      fullCode += "\t" + nodeData.y.code + "\n";
+      continue;
+    }
+    fullCode += "\t" + nodeData.value.code + "\n";
+  }*/
+
+  fullCode += "}";
+  console.log(fullCode);
 }
 </script>
 
--- src/vpnodes/nodes.ts
+++ src/vpnodes/nodes.ts
@@ -5,6 +5,50 @@ import type { Vec3 } from "webgpu-matrix/dist/1.x/vec3";
 
 export const reteSocket = new ClassicPreset.Socket("socket");
 
+export function nodeToVariableDeclaration(node: ClassicPreset.Node) {
+  return "var " + idToVariableName(node.id);
+}
+
+export function idToVariableName(id: string): string {
+  return `ref_${id.substring(0, 5)}`;
+}
+
+function opToName(op: "+" | "-" | "/" | "*" | "%"): string {
+  return op === "+"
+    ? "Add"
+    : op === "-"
+      ? "Subtract"
+      : op === "/"
+        ? "Divide"
+        : op === "*"
+          ? "Multiply"
+          : "Modulo";
+}
+
+function applyOperator(
+  left: number,
+  right: number,
+  op: "+" | "-" | "/" | "*" | "%",
+): number {
+  let result = left;
+
+  if (op === "+") result += right;
+  else if (op === "-") result -= right;
+  else if (op === "/") result /= right;
+  else if (op === "*") result *= right;
+  else result %= right;
+
+  return result;
+}
+
+export class NodeReturn {
+  constructor(
+    public value: any,
+    public code: string,
+    public refId?: string,
+  ) {}
+}
+
 export class NumberNode extends ClassicPreset.Node {
   constructor() {
     super("Number");
@@ -16,25 +60,34 @@ export class NumberNode extends ClassicPreset.Node {
     this.addOutput("value", new ClassicPreset.Output(reteSocket));
   }
 
-  data(): { value: number } {
+  data(): { value: NodeReturn } {
     const control: ClassicPreset.InputControl<"number", number> | undefined =
       this.controls?.value as ClassicPreset.InputControl<"number", number>;
     return {
-      value: control?.value ?? 0,
+      value: {
+        value: control?.value ?? 0,
+        code:
+          nodeToVariableDeclaration(this) +
+          " = " +
+          (control?.value?.toString() ?? "0") +
+          ";",
+        refId: idToVariableName(this.id),
+      },
     };
   }
 }
 
-export class AddNode extends ClassicPreset.Node {
+export class MathOpNode extends ClassicPreset.Node {
   constructor(
+    private operator: "+" | "-" | "/" | "*" | "%",
     private update?: (control: ClassicPreset.InputControl<"number">) => void,
   ) {
-    super("Add");
+    super(opToName(operator));
     this.addInput("left", new ClassicPreset.Input(reteSocket, "X"));
     this.addInput("right", new ClassicPreset.Input(reteSocket, "Y"));
 
     this.addControl(
-      "sum",
+      "result",
       new ClassicPreset.InputControl("number", {
         readonly: true,
         initial: 0,
@@ -43,20 +96,63 @@ export class AddNode extends ClassicPreset.Node {
     this.addOutput("value", new ClassicPreset.Output(reteSocket, "Result"));
   }
 
-  data(inputs: { left?: number[]; right?: number[] }): { value: number } {
+  data(inputs: {
+    left?: NodeReturn[] | number[];
+    right?: NodeReturn[] | number[];
+  }): { value: NodeReturn } {
     const { left, right } = inputs;
-    const value = (left ? left[0] : 0) + (right ? right[0] : 0);
+
     console.log(left);
     console.log(right);
 
+    let leftValue = 0;
+    let leftId = "";
+    if (left) {
+      if (typeof left[0] != "number") {
+        leftValue = left[0].value;
+        leftId = left[0].refId ? left[0].refId : "";
+      } else {
+        leftValue = left[0];
+      }
+    }
+
+    let rightValue = 0;
+    let rightId = "";
+    if (right) {
+      if (typeof right[0] != "number") {
+        rightValue = right[0].value;
+        rightId = right[0].refId ? right[0].refId : "";
+      } else {
+        rightValue = right[0];
+      }
+    }
+
+    console.log(leftValue);
+    console.log(rightValue);
+
+    const value = applyOperator(leftValue, rightValue, this.operator);
+    const code =
+      nodeToVariableDeclaration(this) +
+      " = " +
+      (leftId === "" ? leftValue.toString() : leftId) +
+      ` ${this.operator} ` +
+      (rightId === "" ? rightValue.toString() : rightId) +
+      ";";
+    console.log(value);
+
     const control: ClassicPreset.InputControl<"number", number> | undefined =
-      this.controls?.sum as ClassicPreset.InputControl<"number", number>;
-    console.log(control);
+      this.controls?.result as ClassicPreset.InputControl<"number", number>;
     control?.setValue(value);
 
     if (this.update) this.update(control);
 
-    return { value };
+    return {
+      value: {
+        value: value,
+        code: code,
+        refId: idToVariableName(this.id),
+      },
+    };
   }
 }
 
@@ -87,10 +183,10 @@ export class Vector2Node extends ClassicPreset.Node {
       }),
     );
 
-    this.addOutput("vec", new ClassicPreset.Output(reteSocket));
+    this.addOutput("value", new ClassicPreset.Output(reteSocket));
   }
 
-  data(): { vec: Vec2 } {
+  data(): { value: NodeReturn } {
     const xCont: ClassicPreset.InputControl<"number", number> | undefined = this
       .controls?.x as ClassicPreset.InputControl<"number", number>;
     const yCont: ClassicPreset.InputControl<"number", number> | undefined = this
@@ -107,7 +203,20 @@ export class Vector2Node extends ClassicPreset.Node {
     );
     if (this.update) this.update(vecCont);
 
-    return { vec: vec2.create(xCont?.value ?? 0, yCont?.value ?? 0) };
+    return {
+      value: {
+        value: vec2.create(xCont?.value ?? 0, yCont?.value ?? 0),
+        code:
+          nodeToVariableDeclaration(this) +
+          " = " +
+          "vec2(" +
+          (xCont?.value?.toString() ?? "0") +
+          ", " +
+          (yCont?.value?.toString() ?? "0") +
+          ");",
+        refId: idToVariableName(this.id),
+      },
+    };
   }
 }
 
@@ -123,12 +232,20 @@ export class Seperate2Node extends ClassicPreset.Node {
     this.addOutput("y", new ClassicPreset.Output(reteSocket, "Y"));
   }
 
-  data(inputs: { vector?: Vec2 }): { x: number; y: number } {
+  data(inputs: { vector?: NodeReturn[] }): { x: NodeReturn; y: NodeReturn } {
     const vector = inputs.vector;
-    console.log(vec2.len(vector ?? vec2.zero()));
+    console.log(vector ? vector[0].value : "Hello");
     return {
-      x: vector ? vector[0][0] : 0,
-      y: vector ? vector[0][1] : 0,
+      x: {
+        value: vector ? vector[0].value[0] : 0,
+        code: `${nodeToVariableDeclaration(this)}_1 = ${vector ? vector[0].refId + "[0]" : "0"};`,
+        refId: idToVariableName(this.id) + "_1",
+      },
+      y: {
+        value: vector ? vector[0].value[1] : 0,
+        code: `${nodeToVariableDeclaration(this)}_2 = ${vector ? vector[0].refId + "[1]" : "0"};`,
+        refId: idToVariableName(this.id) + "_2",
+      },
     };
   }
 }
@@ -142,13 +259,20 @@ export class Join2Node extends ClassicPreset.Node {
     this.addInput("x", new ClassicPreset.Input(reteSocket, "X"));
     this.addInput("y", new ClassicPreset.Input(reteSocket, "Y"));
 
-    this.addOutput("vector", new ClassicPreset.Output(reteSocket, "Vector2"));
+    this.addOutput("value", new ClassicPreset.Output(reteSocket, "Vector2"));
   }
 
-  data(inputs: { x?: number; y?: number }): { vector: Vec2 } {
+  data(inputs: { x?: NodeReturn[]; y?: NodeReturn[] }): { value: NodeReturn } {
     const { x, y } = inputs;
     return {
-      vector: vec2.create(x ?? 0, y ?? 0),
+      value: {
+        value: [vec2.create(x ? x[0].value : 0, y ? y[0].value : 0)],
+        code:
+          nodeToVariableDeclaration(this) +
+          " = " +
+          `vec2(${x ? x[0].refId : 0}, ${y ? y[0].refId : 0});`,
+        refId: idToVariableName(this.id),
+      },
     };
   }
 }
@@ -186,10 +310,10 @@ export class Vector3Node extends ClassicPreset.Node {
       }),
     );
 
-    this.addOutput("vec", new ClassicPreset.Output(reteSocket));
+    this.addOutput("value", new ClassicPreset.Output(reteSocket));
   }
 
-  data(): { vec: Vec3 } {
+  data(): { value: NodeReturn } {
     const xCont: ClassicPreset.InputControl<"number", number> | undefined = this
       .controls?.x as ClassicPreset.InputControl<"number", number>;
     const yCont: ClassicPreset.InputControl<"number", number> | undefined = this
@@ -211,7 +335,18 @@ export class Vector3Node extends ClassicPreset.Node {
     if (this.update) this.update(vecCont);
 
     return {
-      vec: vec3.create(xCont?.value ?? 0, yCont?.value ?? 0, zCont?.value ?? 0),
+      value: {
+        value: vec3.create(
+          xCont?.value ?? 0,
+          yCont?.value ?? 0,
+          zCont?.value ?? 0,
+        ),
+        code:
+          nodeToVariableDeclaration(this) +
+          " = " +
+          `vec3(${xCont?.value?.toString() ?? 0}, ${yCont?.value?.toString() ?? 0}, ${zCont?.value?.toString() ?? 0});`,
+        refId: idToVariableName(this.id),
+      },
     };
   }
 }
