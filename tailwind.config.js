5978b063f624c4ec54e8dac32d78b1795704822e
Stefnotch <brandmairstefan@gmail.com>
2025-03-04 19:20:26 +0100

    Fix dark mode

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  darkMode: "selector",
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
